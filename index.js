ec418813e252aa9926b2707672eb380a5e585d7a
Sarah.Keller <sarah29.84@gmail.com>
2023-06-10 11:52:06 +0200

    Updated movie list, typos in documentation

--- index.js
+++ index.js
@@ -13,100 +13,74 @@ app.use(bodyParser.json());
 
 let users = [
   {
-    userName: '',
-    email: '',
-    favoriteMovie: []
+    userName: 'JackBNimble',
+    email: 'j.nimble@madeup.com',
+    favoriteMovie: ['Princess Bride']
+  },
+
+  {
+    userName: 'JillTree',
+    email: 'jtree@madeup.comm',
+    favoriteMovie: ['Harry Potter and the Philosopher\'s Stone']
+  },
+
+  {
+    userName: 'HumptyDumpty',
+    email: 'humpty@allthekingsmen.com',
+    favoriteMovie: ['Mulan']
   }
 ];
 
 let topMovies = [
   {
-    title: 'Harry Potter and the Sorcerer\'s Stone',
+    title: 'Harry Potter and the Philosopher\'s Stone',
     genre: {
-      genreName: '',
-      description: ''
+      genreName: 'Fantasy',
+      description: 'Stories that contain elements that are not realistic'
     },
     director: {
-      name: 'J.K. Rowling',
-      bio: '',
-      birth: '',
-      death: ''
+      name: 'Chris Columbus',
+      bio: 'American film director, producer, and screenwriter',
+      birth: 'September 10, 1958',
+      death: '-'
     },
-    imageURL: ''
+    description: `Harry Potter and the Philosopher's Stone" is a fantasy film based on J.K. Rowling's popular book series. The story follows Harry Potter, an orphaned boy who discovers he is a wizard and is invited to attend Hogwarts School of Witchcraft and Wizardry. Alongside his new friends Ron and Hermione, Harry uncovers dark secrets, battles magical creatures, and encounters the evil wizard Lord Voldemort. It's a captivating tale of friendship, adventure, and the beginning of Harry's journey to becoming a legendary wizard.`,
+    imageURL: `https://en.wikipedia.org/wiki/Harry_Potter_and_the_Philosopher%27s_Stone_%28film%29#/media/File:Harry_Potter_and_the_Philosopher's_Stone_banner.jpg`,
+    featured: true
   },
 
   {
-    title: 'Harry Potter and the Sorcerer\'s Stone',
+    title: 'Mulan',
     genre: {
-      genreName: '',
-      description: ''
+      genreName: 'Animated',
+      description: 'Films in which drawings are used in a way that makes them move as if they were alive'
     },
+    description: `"Mulan" is a thrilling and inspiring animated film from Disney. The story follows Mulan, a young Chinese woman who disguises herself as a man to take her father's place in the Imperial Army. With courage and determination, Mulan faces challenges and fights against the invading Huns, showcasing her bravery and resourcefulness. The film explores themes of gender identity, honor, and family while highlighting Mulan's journey of self-discovery and her quest to bring honor to her family. "Mulan" is a captivating tale of empowerment, resilience, and the importance of staying true to oneself.`,
     director: {
-      name: 'J.K. Rowling',
-      bio: '',
-      birth: '',
-      death: ''
+      name: 'Barry Cook',
+      bio: 'American film director in the animation industry',
+      birth: '12 August 1958',
+      death: '-'
     },
-    imageURL: ''
-  },
-
-  {
-    title: 'Harry Potter and the Sorcerer\'s Stone',
-    genre: {
-      genreName: '',
-      description: ''
-    },
-    director: {
-      name: 'J.K. Rowling',
-      bio: '',
-      birth: '',
-      death: ''
-    },
-    imageURL: ''
-  },
-
-  {
-    title: 'Lord of the Rings',
-    author: 'J.R.R. Tolkien'
-  },
-  {
-    title: 'Twilight',
-    author: 'Stephanie Meyer'
+    imageURL: 'https://upload.wikimedia.org/wikipedia/en/a/a3/Movie_poster_mulan.JPG',
+    featured: true
   },
 
   {
     title: 'The Princess Bride',
-    director: 'William Goldman'
-  },
-
-  {
-    title: 'Jane Eyre',
-    director: 'Charlotte Bronte'
-  },
-
-  {
-    title: 'Persuasion',
-    director: 'Jane Austen'
-  },
-
-  {
-    title: 'The Emperor\'s new Groove',
-    director: 'Walt Disney'
-  },
-
-  { 
-    title: 'Mulan',
-    director: 'Walt Disney'
-  },
-
-  {
-    title: 'Enola Holmes',
-    director: 'Nancy Springer'
-  },
-
-  {
-    title: 'Miss Fisher\'s Murder Mysteries',
-    director: 'Kerry Greenwood'
+    genre: {
+      genreName: 'Fantasy',
+      description: 'Films that contain elements that are not realistic',
+    },
+    description: `"The Princess Bride" is a beloved fantasy-adventure film that combines romance, humor, and swashbuckling action. The story revolves around a young woman named Buttercup and her true love, Westley. Separated by fate, they face numerous obstacles, including a vengeful prince, a clever swordsman, and a giant with a kind heart. Filled with witty dialogue, memorable characters, and a timeless love story, "The Princess Bride" is a charming and whimsical tale that has enchanted audiences for decades.`,
+    director: {
+      name: 'Rob Reiner',
+      bio: 'American actor and filmmaker',
+      birth: 'March 6, 1947',
+      death: '-'
+    },
+    imageURL: `https://en.wikipedia.org/wiki/The_Princess_Bride_(film)#/media/File:Princess_bride.jpg`,
+    featured: true
   }
 ];
 
