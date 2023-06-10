5f9f9a1cb21c4d18fb593f96ad2d8889748e3d68
Sarah.Keller <sarah29.84@gmail.com>
2023-06-10 13:57:15 +0200

    Updated request/response types, corrected index.js missing routes

--- index.js
+++ index.js
@@ -85,16 +85,118 @@ let topMovies = [
 ];
 
 // GET requests
+
+// Gets the default page
 app.get('/', (req, res) => {
   res.send('Welcome to the flixApp!');
 });
 
+// Gets the documentation page
 app.get('/documentation', (req, res) => {                  
   res.sendFile('public/documentation.html', { root: __dirname });
 });
 
+// Gets the data about all movies
 app.get('/movies', (req, res) => {
   res.json(topMovies);
+});
+
+// Gets the data about all users
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+// Gets the data about a single movie, by title
+app.get('/movies/:title', (req, res) => {
+  res.json(
+    topMovies.find((movie) => {
+      return movie.title === req.params.title;
+    })
+  );
+});
+
+// POST requests
+
+// Adds data for a new user to our list of users.
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.userName) {
+    const message = 'Missing username in request body';
+    res.status(400).send(message);
+  } else {
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+// Allow users to add a movie to their list of favorites
+app.post('/users/:email/favorites', (req, res) => {
+  const { email } = req.params;
+  const { movie } = req.body;
+
+  const user = users.find((user) => user.email === email);
+
+  if (user) {
+    user.favoriteMovie.push(movie);
+    res.status(201).send(`Movie "${movie}" has been added to favorites for user with email ${email}.`);
+  } else {
+    res.status(404).send(`User with email ${email} not found.`);
+  }
+});
+
+// PUT requests
+
+// Allow users to update their user info (username)
+app.put('/users/:email', (req, res) => {
+  const { email } = req.params;
+  const { newUsername } = req.body;
+
+  const user = users.find((user) => user.email === email);
+
+  if (user) {
+    user.userName = newUsername;
+    res.status(200).send(`Username for email ${email} has been updated to ${newUsername}.`);
+  } else {
+    res.status(404).send(`User with email ${email} not found.`);
+  }
+});
+
+// DELETE requests
+
+// Deletes a user from our list by email
+app.delete('/users/:userName/:email', (req, res) => {
+  let user = users.find((user) => {
+    return user.email === req.params.email;
+  });
+
+  if (user) {
+    students = users.filter((obj) => {
+      return obj.email !== req.params.email;
+    });
+    res.status(201).send(`Username ${req.params.userName} with email ${req.params.email} was deleted.`);
+  }
+});
+
+// Deletes a movie from the user's favorite list by email
+app.delete('/users/:email/favorites/:movie', (req, res) => {
+  const { email, movie } = req.params;
+
+  const user = users.find((user) => user.email === email);
+
+  if (!user) {
+    res.status(404).send('User not found');
+    return;
+  }
+
+  if (!user.favoriteMovie.includes(movie)) {
+    res.status(404).send('Movie not found in favorites');
+    return;
+  }
+
+  user.favoriteMovie = user.favoriteMovie.filter((favMovie) => favMovie !== movie);
+
+  res.status(200).send(`Movie ${movie} removed from favorites for ${user.userName}.`);
 });
 
 //error-handling
