03b8de24ae73fb4d449c2d16aeffcc63e8ebeaaf
Sarah.Keller <sarah29.84@gmail.com>
2023-06-25 14:27:01 +0200

    Updated app.listen

--- index.js
+++ index.js
@@ -305,6 +305,6 @@ app.use((err, req, res, next) => {
 });
 
 // listen for requests
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+app.listen(port, '0.0.0.0', () => {
+  console.log('Your app is listening on port ' + port);
 });
