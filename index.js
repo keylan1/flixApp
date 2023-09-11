11d92e1642b103ecfe4c2efcab613bca6573a181
Sarah.Keller <sarah29.84@gmail.com>
2023-09-11 21:43:21 +0200

    Added CORS origin

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
 let allowedOrigins = [
   'http://localhost:8080',
+  'http://localhost:4200',
   'http://localhost:1234',
   'http://testsite.com',
   'https://mflixapp.netlify.app',
